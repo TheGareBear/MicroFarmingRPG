10a941f33945972520584d6292c494ec63b09857
GVassar <51205052+TheGareBear@users.noreply.github.com>
2023-11-07 10:23:35 -0700

    Player controls setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Runtime.ExceptionServices;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Vector2 moveInput;
+    private bool interactInput;
+    private Vector2 facingDir;
+    public LayerMask interactLayerMask;
+    public Rigidbody2D rig;
+    public SpriteRenderer sr;
+
+    void Update()
+    {
+        if(moveInput.magnitude != 0.0f)
+        {
+            facingDir = moveInput.normalized;
+            sr.flipX = moveInput.x > 0;
+        }    
+    }
+
+    void FixedUpdate()
+    {
+        rig.velocity = moveInput.normalized * moveSpeed;
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+    
+    public void OnInteractInput(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Performed)
+        {
+            interactInput = true;
+        }
+    }
+}
